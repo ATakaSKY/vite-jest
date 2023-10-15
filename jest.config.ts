bd7da144d10d76c8486759bcce8b6d63c61f7e52
Aakash Thakur <thakurakash321@gmail.com>
2023-10-15 12:52:33 +0530

    jest config

--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,12 @@
 export default {
-    testEnvironment: "jsdom",
-    transform: {
-        "^.+\\.tsx?$": "ts-jest"
-    },
-    moduleNameMapper: {
-        '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__mocks__/fileMock.ts',
-        '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
-    },
-    setupFilesAfterEnv: ['<rootDir>/jest.setup.ts']
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__mocks__/fileMock.ts',
+    '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testMatch: ['**/__tests__/**/*.[jt]s?(x)'],
 }
